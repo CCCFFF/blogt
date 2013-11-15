e7efd3c5718984fd5fe8fba337bd982a15e6f5f5
Craig <craig.frey@hotmail.com>
2013-11-15 07:28:20 -0600

    added jquery to make homepage columns the same size

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+   function equalHeight(group) {
+   tallest = 0;
+   group.each(function() {
+      thisHeight = $(this).height();
+      if(thisHeight > tallest) {
+         tallest = thisHeight;
+      }
+   });
+   group.height(tallest);
+}
+$(document).ready(function() {
+   equalHeight($(".column"));
+});
